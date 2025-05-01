94f5fbe7f97ae9e09094708f5c0f75225e9948ab
Sunthewhat <chotiwet.wisi@icloud.com>
2025-05-01 23:49:43 +0700

    apply custom class

--- src/components/shared/topBar.tsx
+++ src/components/shared/topBar.tsx
@@ -9,7 +9,7 @@ const TopBar: FC = () => {
 	return (
 		<header className='bg-white shadow'>
 			<div className='max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8 flex justify-between items-center'>
-				<h1 className='text-2xl font-bold text-gray-900'>My Auth App</h1>
+				<h1 className='text-2xl font-bold text-gray-900 font-adlam'>SecureDocs</h1>
 
 				{/* Auth status */}
 				<div>
