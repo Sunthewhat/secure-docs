5117183fd5f925a6cc3da7ce4b9c3b318c784d28
Thawatchai Wongboonsiri <jj.wbsr@gmail.com>
2025-05-12 22:45:06 +0700

    [FIX] comment this file

--- src/components/home/collection.tsx
+++ src/components/home/collection.tsx
@@ -1,13 +1,13 @@
-import { FC, useState } from "react";
+// import { FC, useState } from "react";
 
-type CollectionProp = {
-	name: string;
-};
-const Collection: FC<CollectionProp> = (name) => {
-	const [collection, setCollection] = useState<CollectionProp>();
-	const handleCollection = () => {
-		setCollection(name);
-	};
-	return <></>;
-};
-export { Collection };
+// type CollectionProp = {
+// 	name: string;
+// };
+// const Collection: FC<CollectionProp> = (name) => {
+// 	const [collection, setCollection] = useState<CollectionProp>();
+// 	const handleCollection = () => {
+// 		setCollection(name);
+// 	};
+// 	return <></>;
+// };
+// export { Collection };
