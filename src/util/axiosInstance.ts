8baaf4e29f335d03757bff21a588408ed9ed09d7
Sunthewhat <chotiwet.wisi@icloud.com>
2025-09-01 22:25:57 +0700

    feat/refreshAutoken

--- src/util/axiosInstance.ts
+++ src/util/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const Axios: AxiosInstance = axios.create({
-	baseURL: 'https://easy-cert-api.sunthewhat.com/api/v1',
+	baseURL: `${import.meta.env.VITE_BACKEND_API}/api/v1`,
 	withCredentials: true,
 	validateStatus: (status) => status >= 200 && status <= 500,
 	headers: {
@@ -25,6 +25,15 @@ Axios.interceptors.request.use(
 	(error) => {
 		return Promise.reject(error);
 	}
+);
+
+Axios.interceptors.response.use(
+	(response) => {
+		const token = response.headers['x-refresh-token'];
+		localStorage.setItem('authToken', token);
+		return response;
+	},
+	(error) => Promise.reject(error)
 );
 
 export { Axios };
